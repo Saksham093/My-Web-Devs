b4e777d8d57e57b7248c8c4ceec71d1f695099f5
M.SAKSHAM <69385787+Saksham093@users.noreply.github.com>
2024-12-20 01:37:41 +0530

    Delete Projects/Movie_Ticket_Booking directory

--- Projects/Movie_Ticket_Booking/js/details.js
+++
@@ -1,5 +0,0 @@
-// details.js
-const movieDetails = {
-    name: "Aavesham",
-    time: "Sun 28 Apr'24 - 11:30 AM"
-};
--- Projects/Movie_Ticket_Booking/js/script.js
+++
@@ -1,6 +0,0 @@
-function togglePopup() {
-    var popup = document.getElementById("popup");
-    // console.log("Toggle function called!");
-    popup.style.display = popup.style.display === 'none' ? 'block' : 'none';
-  }
-  
